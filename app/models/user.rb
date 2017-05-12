db8e247e93156865ac843abeab4f26207c5357f5
JongHa <dbswhdgk93@gmail.com>
2017-05-12 13:15:28 +0000

    2017/5/12 devise/rolify/will_paginate

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  
+  after_create :set_default_role, if: Proc.new { User.count > 1 }
+
+
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
